86e37b2e706e04b01ee4737e5447cbeb1a052de8
sammytam0430 <sammytam2005@hotmail.com>
2019-05-02 15:31:35 -0700

    created sign up button

---
+++ client/src/components/SignUpBtn.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="signUpBtn">
+        <b-button variant="outline-primary">Sign Up</b-button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SignUpBtn"
+}
+</script>
