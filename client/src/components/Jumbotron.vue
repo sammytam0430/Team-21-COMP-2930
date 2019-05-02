01896ff8d918fbbb54d3ee2f26fbe96dc8d21464
sammytam0430 <sammytam2005@hotmail.com>
2019-05-02 16:34:39 -0700

    created jumbotron for home page

---
+++ client/src/components/Jumbotron.vue
@@ -0,0 +1,13 @@
+<template>
+    <b-jumbotron header="Connect with Konnect" lead="Start looking for people to do things with">
+        <b-button variant="primary" href="#">More Info</b-button>
+    </b-jumbotron>
+</template>
+
+<style scoped>
+    .jumbotron {
+        background-image: url("../assets/background.jpg");
+        background-size: cover;
+    }
+</style>
+
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="home">
+    <Jumbotron/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Jumbotron from '@/components/Jumbotron.vue';
 
 export default {
+
   name: 'home',
   components: {
+    Jumbotron
   }
 }
 </script>
