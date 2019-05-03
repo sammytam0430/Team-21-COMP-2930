75d577a93c71acf1abafadb2d5994f6972ca2b55
Jameson Cheong <jctheathlete@gmail.com>
2019-05-03 12:33:02 -0700

    add dashboard and change router

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Posts from './components/Posts.vue'
+import DashBoard from './views/DashBoard.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/posts',
       name: 'Posts',
       component: Posts
+    },
+    {
+      path: '/dashboard',
+      name: 'Dashboard',
+      component: DashBoard
     }
   ]
 })
---
+++ client/src/views/DashBoard.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="DashBoard">
+    <b-container >
+    <b-row>
+        <b-col cols="12" md="8">
+            <b-row>
+                <b-col>Near By Events</b-col>
+                <b-col><b-button size="sm">Create Event</b-button></b-col>
+            </b-row>
+            <b-row>
+                <b-col>
+                    <div id="myMap"></div>
+                </b-col>
+            </b-row>
+        </b-col>
+        <b-col cols="6" md="4">
+            <b-row>
+                <b-col>Near By Events</b-col>
+                <b-col><b-button size="sm">Search</b-button></b-col>
+            
+                <b-table striped hover :items="Events"></b-table>
+
+            </b-row>
+              <div>
+    <b-table :items="items" :fields="fields" striped>
+      <template slot="online" slot-scope="row">
+        <b-form-checkbox v-model="row.detailsShowing" @change="row.toggleDetails">
+        </b-form-checkbox>
+        <span v-bind:class="[row.item.online ? 'onlineStyle' : 'offlineStyle']"></span>
+      </template>
+    </b-table>
+  </div>
+        </b-col>
+    </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+      return {
+        Events: [
+          {Event: "Basketball", People: "2/4"},
+          {Event: "BaseBall", People: "2/20"},
+          {Event: "Soccer", People: "2/10"},
+          {Event: "Yelling", People: "2/5"}
+        ],
+        fields: ['friend', 'online'],
+        items: [
+          { friend: 'Dickerson',
+            online: true},
+          { friend: 'Larsen',
+            online: false},
+          {
+            friend: 'Geneva',
+            online: false
+          },
+          { friend: 'Jami',
+            online: true}
+        ]
+      }
+    }
+}
+</script>
+
+<style>
+.offlineStyle {
+  height: 15px;
+  width: 15px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+}
+.onlineStyle {
+  height: 15px;
+  width: 15px;
+  background-color: rgb(41, 248, 0);
+  border-radius: 50%;
+  display: inline-block;
+}
+</style>
+
+
+
+
