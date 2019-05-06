0bf1445ffb03e7806a08647fce77e8af42240a25
sammytam0430 <sammytam2005@hotmail.com>
2019-05-06 12:21:25 -0700

    added recent listing component to Home view

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <Jumbotron/>
+    <RecentListing/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Jumbotron from '@/components/Jumbotron.vue';
+import RecentListing from '@/components/RecentListing.vue';
 
 export default {
 
   name: 'home',
   components: {
-    Jumbotron
+    Jumbotron,
+    RecentListing
   }
 }
 </script>
