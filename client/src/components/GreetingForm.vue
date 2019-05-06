c9f038d5cf7e81e603c4980fcfc3ff82b1198b43
sammytam0430 <sammytam2005@hotmail.com>
2019-05-06 12:23:07 -0700

    added now checkbox for form

--- client/src/components/GreetingForm.vue
+++ client/src/components/GreetingForm.vue
@@ -10,11 +10,11 @@
             <b-container class="px-0">
                 <b-form-row>
                     <b-col>
-                            <b-form-input id="when" v-model="form.date" type="date" required placeholder="mm-dd-yyyy"></b-form-input>
+                        <b-form-input id="when" v-model="form.date" type="date" :disabled="now === 'true'" required placeholder="mm-dd-yyyy"></b-form-input>
                     </b-col>
                     <b-col cols="3">
-                        <b-form-checkbox v-on:checked="now = !now" id="checkbox-1" v-model="now" name="checkbox-1" value="true" unchecked-value="false"> 
-                            Now 
+                        <b-form-checkbox v-on:change="setTime()" id="checkbox-1" v-model="now" name="checkbox-1" value="true" unchecked-value="false"> 
+                            Now
                         </b-form-checkbox>
                     </b-col>
                 </b-form-row>
@@ -24,7 +24,7 @@
             <b-form-row>
                 <b-col>
                     <b-form-group id="start-group" label="Start" label-for="start">
-                        <b-form-input id="start" v-model="form.start" type="time" required placeholder="mm/dd/yyyy"></b-form-input>
+                        <b-form-input id="start" v-model="form.start" type="time" :disabled="now === 'true'" required placeholder="mm/dd/yyyy"></b-form-input>
                     </b-form-group>
                 </b-col>
                 <b-col>
@@ -34,7 +34,7 @@
                 </b-col>
             </b-form-row>
         </b-container>
-        <b-button type="submit" variant="primary">Search</b-button>
+        <b-button type="submit" variant="primary">Create Event</b-button>
     </b-form>
 </template>
 
@@ -51,6 +51,18 @@ export default {
                 name: '',
                 start: ''
             }
+        }
+    },
+    methods: {
+        setTime() {
+            let now = new Date();
+            let year = now.getFullYear();
+            let month = now.getMonth() > 9 ? now.getMonth() + 1 : '0' + (now.getMonth() + 1);
+            let date = now.getDate() > 9 ? now.getDate() + 1 : '0' + (now.getDate() + 1);
+            let hour = now.getHours() > 10 ? now.getHours() : '0' + now.getDate();
+            let minute = now.getMinutes() > 10 ? now.getMinutes() : '0' + now.getDate();
+            this.form.date = year + '-' + month + '-' + date;
+            this.form.start = hour + ':' + minute;
         }
     }
 }
