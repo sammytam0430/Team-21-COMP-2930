1975965177dc39e32b6737ec4b0e9f366e271303
sammytam0430 <sammytam2005@hotmail.com>
2019-05-07 10:38:46 -0700

    modified components to fetch data from api

--- client/src/components/RecentListing.vue
+++ client/src/components/RecentListing.vue
@@ -2,44 +2,38 @@
   <div class="pb-5 px-5">
     <h1 class="text-center py-5">Recent Events</h1>
     <b-card-group columns>
-      <b-card :key="post.id" v-for="post in listings" :title="post.title">
-        <b-card-text>
-          {{ post.desc }}
-        </b-card-text>
-        <div slot="footer"><small class="text-muted">Last updated 3 mins ago</small></div>
+      <b-card :key="event.eventID" v-for="event in events" :title="event.name">
+        <b-card-text>{{ event.description }}</b-card-text>
+        <div slot="footer">
+          <Timestamp :time="event.created_at"/>
+        </div>
       </b-card>
     </b-card-group>
   </div>
 </template>
 
 <script>
+import EventsService from "@/services/EventsService";
+import Timestamp from "@/components/Timestamp.vue";
+
 export default {
-    name: 'recentListing',
-    data() {
-        return {
-            listings: [
-                {
-                    id: 1,
-                    title: 'Need people for boardgame',
-                    desc: 'Got 3 hours break and wanted to play some boardgame, come join me!!!'
-                },
-                {
-                    id: 2,
-                    title: 'Shoot around at the gym',
-                    desc: 'Gonna go shoot around, join me if you want.'
-                },
-                {
-                  id: 3,
-                  title: 'Workout buddy needed',
-                  desc: 'Come workout with me'
-                },
-                {
-                  id: 4,
-                  title: 'Join us for impromtu movie session',
-                  desc: 'We\'re watching Captain Marvel, come hang out with us'
-                }
-            ]
-        }
+  name: "recentListing",
+  components: {
+    Timestamp
+  },
+  data() {
+    return {
+      events: {}
+    };
+  },
+  mounted() {
+    this.loadEvents();
+  },
+  methods: {
+    async loadEvents() {
+      const response = await EventsService.getEvents();
+      this.events = response.data;
     }
-}
+  }
+};
 </script>
