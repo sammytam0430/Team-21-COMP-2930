961e07faa2a6201a076f003727b30a3b0c3e1d19
Charles <charleshuang97@gmail.com>
2019-05-09 15:41:36 -0700

    get event type from database to createEvent page

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const events = require('./events.js')
 const users = require('./users.js')
+const interests = require('./interests.js')
 
 router.use('/events', events);
 router.use('/users', users);
+router.use('/interests', interests);
 
 module.exports = router;
