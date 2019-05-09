7f3f3cbc844e7106ef42501e81006cdfd9577e3b
sammytam0430 <sammytam2005@hotmail.com>
2019-05-09 13:47:18 -0700

    implemented bcrypt on user login

--- client/src/components/GreetingForm.vue
+++ client/src/components/GreetingForm.vue
@@ -1,69 +1,91 @@
 <template>
-    <b-form  v-on:submit.prevent="">
-        <b-form-group id="what-group" label="I'm looking for people to" label-for="what">
-            <b-form-input id="what" v-model="form.name" type="text" required placeholder="Anything"></b-form-input>
-        </b-form-group>
-        <b-form-group id="where-group" label="Where" label-for="where">
-            <b-form-input id="where" v-model="form.location" type="text" required placeholder="Anywhere"></b-form-input>
-        </b-form-group>
-        <b-form-group id="when-group" label="When" label-for="when">
-            <b-container class="px-0">
-                <b-form-row>
-                    <b-col>
-                        <b-form-input id="when" v-model="form.date" type="date" :disabled="now === 'true'" required placeholder="mm-dd-yyyy"></b-form-input>
-                    </b-col>
-                    <b-col cols="3">
-                        <b-form-checkbox v-on:change="setTime()" id="checkbox-1" v-model="now" name="checkbox-1" value="true" unchecked-value="false"> 
-                            Now
-                        </b-form-checkbox>
-                    </b-col>
-                </b-form-row>
-            </b-container>
-        </b-form-group>
-        <b-container class="px-0">
-            <b-form-row>
-                <b-col>
-                    <b-form-group id="start-group" label="Start" label-for="start">
-                        <b-form-input id="start" v-model="form.start" type="time" :disabled="now === 'true'" required placeholder="mm/dd/yyyy"></b-form-input>
-                    </b-form-group>
-                </b-col>
-                <b-col>
-                    <b-form-group id="end-group" label="End" label-for="end">
-                        <b-form-input id="end" v-model="form.end" type="time" required placeholder="mm/dd/yyyy"></b-form-input>
-                    </b-form-group>
-                </b-col>
-            </b-form-row>
-        </b-container>
-        <b-button type="submit" variant="primary">Create Event</b-button>
-    </b-form>
+  <b-form v-on:submit.prevent>
+    <b-form-group id="what-group" label="I'm looking for people to" label-for="what">
+      <b-form-input id="what" v-model="form.name" type="text" required placeholder="Anything"></b-form-input>
+    </b-form-group>
+    <b-form-group id="where-group" label="Where" label-for="where">
+      <b-form-input id="where" v-model="form.location" type="text" required placeholder="Anywhere"></b-form-input>
+    </b-form-group>
+    <b-form-group id="when-group" label="When" label-for="when">
+      <b-container class="px-0">
+        <b-form-row>
+          <b-col>
+            <b-form-input
+              id="when"
+              v-model="form.date"
+              type="date"
+              :disabled="now === 'true'"
+              required
+              placeholder="mm-dd-yyyy"
+            ></b-form-input>
+          </b-col>
+          <b-col cols="3">
+            <b-form-checkbox
+              v-on:change="setTime()"
+              id="checkbox-1"
+              v-model="now"
+              name="checkbox-1"
+              value="true"
+              unchecked-value="false"
+            >Now</b-form-checkbox>
+          </b-col>
+        </b-form-row>
+      </b-container>
+    </b-form-group>
+    <b-container class="px-0">
+      <b-form-row>
+        <b-col>
+          <b-form-group id="start-group" label="Start" label-for="start">
+            <b-form-input
+              id="start"
+              v-model="form.start"
+              type="time"
+              :disabled="now === 'true'"
+              required
+              placeholder="mm/dd/yyyy"
+            ></b-form-input>
+          </b-form-group>
+        </b-col>
+        <b-col>
+          <b-form-group id="end-group" label="End" label-for="end">
+            <b-form-input id="end" v-model="form.end" type="time" required placeholder="mm/dd/yyyy"></b-form-input>
+          </b-form-group>
+        </b-col>
+      </b-form-row>
+    </b-container>
+    <b-button type="submit" variant="primary">Create Event</b-button>
+  </b-form>
 </template>
 
 <script>
 export default {
-    name: 'greetingForm',
-    data() {
-        return {
-            now: false,
-            form: {
-                date: '',
-                end: '',
-                location: '',
-                name: '',
-                start: ''
-            }
-        }
-    },
-    methods: {
-        setTime() {
-            let now = new Date();
-            let year = now.getFullYear();
-            let month = now.getMonth() > 9 ? now.getMonth() + 1 : '0' + (now.getMonth() + 1);
-            let date = now.getDate() > 9 ? now.getDate() + 1 : '0' + (now.getDate() + 1);
-            let hour = now.getHours() > 10 ? now.getHours() : '0' + now.getDate();
-            let minute = now.getMinutes() > 10 ? now.getMinutes() : '0' + now.getDate();
-            this.form.date = year + '-' + month + '-' + date;
-            this.form.start = hour + ':' + minute;
-        }
+  name: "greetingForm",
+  data() {
+    return {
+      now: false,
+      form: {
+        date: "",
+        end: "",
+        location: "",
+        name: "",
+        start: ""
+      }
+    };
+  },
+  methods: {
+    setTime() {
+      let now = new Date();
+      let year = now.getFullYear();
+      let month =
+        now.getMonth() > 9 ? now.getMonth() + 1 : "0" + (now.getMonth() + 1);
+      let date =
+        now.getDate() > 9 ? now.getDate() + 1 : "0" + (now.getDate() + 1);
+      let hour = now.getHours() > 10 ? now.getHours() : "0" + now.getDate();
+      let minute =
+        now.getMinutes() > 10 ? now.getMinutes() : "0" + now.getDate();
+      this.form.date = year + "-" + month + "-" + date;
+      this.form.start = hour + ":" + minute;
     }
-}
+  }
+};
 </script>
--- client/src/components/SignUpModal.vue
+++ client/src/components/SignUpModal.vue
@@ -1,45 +1,106 @@
 <template>
-    <b-modal id="signup" title="Sign Up" ok-title="Sign Up" ok-only>
-        <b-form>
-            <b-form-group id="name-group" label="Name" label-for="name">
-                <b-form-input id="name" v-model="signup.name" type="text" required></b-form-input>
+  <b-modal id="signup" title="Sign Up">
+    <b-alert :show="response" :variant="response.success ? success : danger">{{ response.message }}</b-alert>
+    <b-form @submit="createAccount()">
+      <b-container class="px-0">
+        <b-form-row>
+          <b-col>
+            <b-form-group id="fname-group" label="First Name" label-for="fname">
+              <b-form-input id="fname" v-model="signup.fname" type="text" required></b-form-input>
             </b-form-group>
-            <b-form-group id="email-group" label="School Email" label-for="email">
-                <b-form-input id="email" v-model="signup.email" type="email" required placeholder="example@my.bcit.ca"></b-form-input>
+          </b-col>
+          <b-col>
+            <b-form-group id="lname-group" label="Last Name" label-for="lname">
+              <b-form-input id="lname" v-model="signup.lname" type="text" required></b-form-input>
             </b-form-group>
-            <b-form-group id="prefEmail-group" label="Preferred Email" label-for="prefEmail" description="We'll never share your email with anyone else.">
-                <b-form-input id="prefEmail" v-model="signup.prefEmail" type="email" required placeholder="example@gmail.com"></b-form-input>
-            </b-form-group>
-            <b-form-group id="password-group" label="Password" label-for="password">
-                <b-form-input id="password" v-model="signup.password" type="password" required></b-form-input>
-            </b-form-group>
-            <b-form-checkbox id="checkbox" name="checkbox" value="true" unchecked-value="false">
-                I agree to the <a href="" v-on:click.prevent >Terms and Conditions</a>
-            </b-form-checkbox>
-        </b-form>
-        <template slot="modal-footer" slot-scope="{ ok }">
-            <span class="mr-2">Already have an account? 
-                <a href="" v-on:click.prevent v-b-modal.login @click="$bvModal.hide('signup')">Log In</a>
-            </span>
-            <b-button variant="primary" @click="ok()">
-                Sign Up
-            </b-button>
-        </template>
-    </b-modal>
+          </b-col>
+        </b-form-row>
+      </b-container>
+      <b-form-group id="email-group" label="School Email" label-for="email">
+        <b-form-input
+          id="email"
+          v-model="signup.email"
+          type="email"
+          required
+          placeholder="example@my.bcit.ca"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="prefEmail-group"
+        label="Preferred Email"
+        label-for="prefEmail"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="prefEmail"
+          v-model="signup.prefEmail"
+          type="email"
+          placeholder="example@gmail.com"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group id="password-group" label="Password" label-for="password">
+        <b-form-input id="password" v-model="signup.password" type="password" required></b-form-input>
+      </b-form-group>
+      <b-form-checkbox
+        id="checkbox"
+        name="checkbox"
+        v-model="agreed"
+        value="true"
+        unchecked-value="false"
+        required
+      >
+        I agree to the
+        <a href v-on:click.prevent>Terms and Conditions</a>
+      </b-form-checkbox>
+      <b-button class="w-100 mt-3" type="submit" variant="primary">Sign Up</b-button>
+    </b-form>
+    <template slot="modal-footer">
+      <span class="mr-auto">
+        Already have an account?
+        <a
+          href
+          v-on:click.prevent
+          v-b-modal.login
+          @click="$bvModal.hide('signup')"
+        >Log In</a>
+      </span>
+    </template>
+  </b-modal>
 </template>
 
 <script>
+import UsersService from "@/services/UsersService";
+
 export default {
-    name: 'signup',
-    data() {
-        return {
-            signup: {
-                email: '',
-                prefEmail: '',
-                name: '',
-                password: ''
-            }
+  name: "SignUpModal",
+  data() {
+    return {
+      response: {
+        success: null,
+        message: {}
+      },
+      agreed: false,
+      signup: {
+        fname: "",
+        lname: "",
+        email: "",
+        prefEmail: "",
+        password: ""
+      }
+    };
+  },
+  methods: {
+    async createAccount() {
+      if (this.agreed) {
+        const response = await UsersService.createUser(this.signup);
+        this.response = response.data;
+        if (response.success) {
+            router.push('dashboard');
+        } else {
+            
         }
+      }
     }
-}
+  }
+};
 </script>
