d2d4e482b37b039487473c0fe338c1f3d8e6e142
sammytam0430 <sammytam2005@hotmail.com>
2019-05-09 14:14:00 -0700

    implemented bcrypt on user authentication

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const events = require('./events.js')
+const users = require('./users.js')
 
 router.use('/events', events);
+router.use('/users', users);
 
 module.exports = router;
