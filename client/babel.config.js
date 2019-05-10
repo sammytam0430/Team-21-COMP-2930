d86ac751a64598d0f580ea7113c89813dfad844e
sammytam0430 <sammytam2005@hotmail.com>
2019-05-10 14:04:46 -0700

    resolve alias

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,5 +1,13 @@
+const path = require('path');
 module.exports = {
   presets: [
     '@vue/app'
-  ]
+  ],
+  resolve: {
+    extensions: ['.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js',
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 }
