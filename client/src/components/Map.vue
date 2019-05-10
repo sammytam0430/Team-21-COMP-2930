81274b44b638b53f80b5192a05dff5365d62380b
Jameson Cheong <jctheathlete@gmail.com>
2019-05-09 22:09:49 -0700

    pushing for hosting

--- client/src/components/Map.vue
+++ client/src/components/Map.vue
@@ -1,103 +1,74 @@
+
 <template>
-    <div>
-  <gmap-map :center="center" :zoom="17" ref="map" style="width: 100%; height: 550px">
-    <gmap-info-window
-      :options="infoOptions"
-      :position="infoPosition"
-      :opened="infoOpened"
-      :content="infoContent"
-      @closeclick="infoOpened=true"
-    ></gmap-info-window>
-    <gmap-marker
-      :key="index"
-      v-for="(m, index) in markers"
-      :position="m.position"
-      :clickable="true"
-      @click="toggleInfo(m, index)"
-    ></gmap-marker>
-  </gmap-map>
-    </div>
+  <div>
+
+    <gmap-map :center="center" :zoom="17" ref="map" style="width: 100%; height: 550px">
+            <GmapMarker v-for="(marker, index) in markers"
+        :key="index"
+        :position="marker.position"
+        />
+      <GmapMarker
+        v-if="this.place"
+        label="★"
+        :position="{
+          lat: this.place.geometry.location.lat(),
+          lng: this.place.geometry.location.lng(),
+        }"
+        />
+    </gmap-map>
+  </div>
 </template>
 
+
 <script>
-import {gmapApi} from 'vue2-google-maps';
+import { gmapApi } from "vue2-google-maps";
+
 export default {
     data() {
+
         return {
-            name: "map",
-                map: null,
-            mapLoaded: false,
-            center: { lat: 49.2500589, lng: -123.0012234 },
             
-            computed: {
-                    google: gmapApi
-                },
-            markers: [
-                {
-                position: {lat: 49.2494589, lng: -123.0028234}
-                
-                },
-                {
-                    position: {lat: 49.249957, lng: -123.001223}
-                }
-        
-            ],
-            infoPosition: null,
-            infoContent: null,
-            infoOpened: false,
-            infoCurrentKey: null,
-            infoOptions: {
-                pixelOffset: {
-                width: 0,
-                height: -35
-                },
-                content:
-                    '<div id="content">'+
-                    '<div id="siteNotice">'+
-                    '</div>'+
-                    '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-                    '<div id="bodyContent">'+
-                    '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-                    'sandstone rock formation in the southern part of the '+
-                    'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-                    'south west of the nearest large town, Alice Springs; 450&#160;km '+
-                    '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-                    'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-                    'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-                    'Aboriginal people of the area. It has many springs, waterholes, '+
-                    'rock caves and ancient paintings. Uluru is listed as a World '+
-                    'Heritage Site.</p>'+
-                    '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-                    'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-                    '(last visited June 22, 2009).</p>'+
-                    '</div>'+
-                    '</div>'
-            }
-        };
-},
+      name: "map",
+      map: null,
+      mapLoaded: false,
+      center: { lat: 49.2500589, lng: -123.0012234 },
+
+      computed: {
+        google: gmapApi
+      },
+      markers: [],
+      place: null,
+
+      infoPosition: null,
+      infoContent: null,
+      infoOpened: false,
+      infoCurrentKey: null,
+      infoOptions: {
+        pixelOffset: {
+          width: 0,
+          height: -35
+        }
+      }
+    };
+  },
   methods: {
-    openInfoWindow(location) {
-      console.log(location);
-      this.selectedLocation = location;
-      this.infoBoxOpen = true;
+    setPlace(place) {
+      this.place = place
     },
-    closeInfoWindow() {
-      this.infoBoxOpen = false;
+    usePlace(place) {
+      if (this.place) {
+        this.markers.push({
+          position: {
+            lat: this.place.geometry.location.lat(),
+            lng: this.place.geometry.location.lng(),
+          }
+        })
+        this.place = null;
+      }
     }
   }
 
-    //   {lat: 49.2511, lng: -123.0}
-    //    markers: [
-    //       {
-    //         name: "BCIT library",
-    //         position: {lat: 49.2494758, lng: -123.0030153}
-    //       },
-    //       {
-    //         name: "BCIT GYM",
-    //         position: {lat: 49.2488123, lng: -123.0033371}
-    //       },
-    //     ],
-    // };
-}
+
+};
 </script>
 
