5102c01f1a074da4d9d8e1988b6918399492f322
sammytam0430 <sammytam2005@hotmail.com>
2019-05-12 20:39:32 -0700

    styled scroll bar

--- client/src/App.vue
+++ client/src/App.vue
@@ -23,4 +23,23 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
 }
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #DDD; 
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #555; 
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #888; 
+}
 </style>
