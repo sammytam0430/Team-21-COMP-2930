7ce70f2120f87fb9d522232c4b5d336e5b40ad2d
sammytam0430 <sammytam2005@hotmail.com>
2019-05-21 19:01:20 -0700

    added feature for adding and removing interests

--- client/src/components/EditInterests.vue
+++ client/src/components/EditInterests.vue
@@ -1,95 +1,74 @@
 <template>
-  <div class="interestselection">
-    <b-modal id="interestmodal" title="Select your interests">
-      <b-container>
-        <b-row>
-          <b-col cols="6" class="px-0">
-            <input type="checkbox" name="checkbox1" id="checkbox1">
-            <label for="checkbox1">
-              <span class="styleCheckbox">&nbsp;&nbsp;Outdoor Sports</span>
-            </label>
-            <br>
-            <input type="checkbox" name="checkbox2" id="checkbox2">
-            <label for="checkbox2">
-              <span class="styleCheckbox">&nbsp;&nbsp;Indoor Sports</span>
-            </label>
-            <br>
-            <input type="checkbox" name="checkbox3" id="checkbox3">
-            <label for="checkbox3">
-              <span class="styleCheckbox">&nbsp;&nbsp;Card/Board Games</span>
-            </label>
-            <br>
-            <input type="checkbox" name="checkbox4" id="checkbox4">
-            <label for="checkbox4">
-              <span class="styleCheckbox">&nbsp;&nbsp;PC Games</span>
-            </label>
-            <br>
-            <input type="checkbox" name="checkbox5" id="checkbox5">
-            <label for="checkbox5">
-              <span class="styleCheckbox">&nbsp;&nbsp;Mobile Games</span>
-            </label>
-            <br>
-          </b-col>
-
-          <b-col class="px-0 mx-0"></b-col>
-
-          <b-col cols="4" class="px-0 float-right">
-            <input type="checkbox" name="checkbox6" id="checkbox6">
-            <label for="checkbox6">
-              <span class="styleCheckbox">&nbsp;&nbsp;Literature</span>
-            </label>
-            <br>
-            <input type="checkbox" name="checkbox7" id="checkbox7">
-            <label for="checkbox7">
-              <span class="styleCheckbox">&nbsp;&nbsp;Music</span>
-            </label>
-            <br>
-            <input type="checkbox" name="checkbox8" id="checkbox8">
-            <label for="checkbox8">
-              <span class="styleCheckbox">&nbsp;&nbsp;Dancing</span>
-            </label>
-            <br>
-            <input type="checkbox" name="checkbox9" id="checkbox9">
-            <label for="checkbox9">
-              <span class="styleCheckbox">&nbsp;&nbsp;Food</span>
-            </label>
-            <br>
-            <input type="checkbox" name="checkbox10" id="checkbox10">
-            <label for="checkbox10">
-              <span class="styleCheckbox">&nbsp;&nbsp;Other</span>
-            </label>
-            <br>
-          </b-col>
-        </b-row>
-      </b-container>
-      <template slot="modal-footer" slot-scope="{ ok, cancel }">
-        <b-button size="sm" variant="danger" @click="cancel()">Cancel</b-button>
-        <b-button size="sm" variant="primary" @click="ok()">OK</b-button>
-      </template>
-    </b-modal>
-  </div>
+  <b-modal id="interestmodal" title="Select your interests">
+    <b-form-checkbox-group id="interest-group" v-model="selected" name="interests" stacked>
+      <b-form-checkbox
+        class="h5"
+        :key="interest.interestID"
+        v-for="interest in interests"
+        :value="interest.interestID"
+        @change="changeInterest(interest.interestID)"
+      >{{ interest.name }}</b-form-checkbox>
+    </b-form-checkbox-group>
+    <template slot="modal-footer" slot-scope="{ ok }">
+      <b-button size="sm" variant="primary" @click="ok()">OK</b-button>
+    </template>
+  </b-modal>
 </template>
 
 <script>
+import InterestsService from "@/services/InterestsService";
+
 export default {
-  name: "interestselection"
+  name: "interestselection",
+  props: ["userInterest"],
+  data() {
+    return {
+      interests: [],
+      selected: []
+    };
+  },
+  watch: {
+    userInterest() {
+      this.selected = this.userInterest.map(interest => interest.interestID);
+    }
+  },
+  mounted() {
+    this.getInterests();
+    this.selected = this.userInterest.map(interest => interest.interestID);
+  },
+  methods: {
+    async getInterests() {
+      const response = await InterestsService.getInterests();
+      this.interests = response.data;
+    },
+    async changeInterest(interestID) {
+      const interestExist = this.selected.includes(interestID);
+      if (!interestExist) {
+        const data = {
+          userID: this.$route.params.id,
+          friendID: interestID
+        };
+        const response = await InterestsService.addUserInterest(data);
+        this.$bvToast.toast(response.data.message, {
+          title: "Notification",
+          toaster: "b-toaster-bottom-right",
+          autoHideDelay: 5000,
+          appendToast: true
+        });
+      } else {
+        const response = await InterestsService.deleteUserInterest(
+          this.$route.params.id,
+          interestID
+        );
+        this.$bvToast.toast(response.data.message, {
+          title: "Notification",
+          toaster: "b-toaster-bottom-right",
+          autoHideDelay: 5000,
+          appendToast: true
+        });
+      }
+      this.$parent.getInterests();
+    }
+  }
 };
 </script>
-
-<style scoped>
-.styleCheckbox {
-  font-size: 20px;
-}
-
-@media only screen and (min-width: 321px) and (max-width: 426px) {
-  .styleCheckbox {
-    font-size: 16px;
-  }
-}
-
-@media only screen and (max-width: 320px) {
-  .styleCheckbox {
-    font-size: 13px;
-  }
-}
-</style>
--- server/routes/api/interests.js
+++ server/routes/api/interests.js
@@ -10,4 +10,39 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  db("userInterest")
+    .join("interests", { "interests.interestID": "userInterest.interestID" })
+    .where({ userID: req.params.id })
+    .then(data => {
+      res.send(data);
+    });
+});
+
+router.post("/", (req, res) => {
+  db.insert([{ userID: req.body.userID, interestID: req.body.friendID }])
+    .returning("*")
+    .into("userInterest")
+    .then(data => {
+      res.json({
+        status: 201,
+        success: true,
+        message: "Added interest successfully"
+      });
+    });
+});
+
+router.delete("/:userID/:interestID", (req, res) => {
+  db("userInterest")
+    .where({ userID: req.params.userID, interestID: req.params.interestID })
+    .del()
+    .then(() => {
+      res.json({
+        status: 200,
+        success: true,
+        message: "Removed interest successfully"
+      });
+    });
+});
+
 module.exports = router;
