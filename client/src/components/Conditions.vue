f4b5bae4c15092829b997182b58732c701dac7a2
Sammy Tam <sammytam2005@hotmail.com>
2019-05-22 01:40:14 -0700

    Update Conditions.vue

--- client/src/components/Conditions.vue
+++ client/src/components/Conditions.vue
@@ -1,6 +1,6 @@
 <template>
   <b-modal id="conditions" title="Terms and Conditions" ok-only>
     <p class="h4">Welcome to Konnect!</p>
-    <p>Let's be real you're not gonna read this, just check the checkbox lol</p>
+    <p>Let's be reaslistic about this. You're not going to even read this, so there's really no point forwarding you to the seven pages of terms and conditions. You now hereby agree to become our slaves for the day. Just check the checkbox and stop wasting time!</p>
   </b-modal>
 </template>
