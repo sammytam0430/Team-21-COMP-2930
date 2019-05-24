c5ac49c3e5d1b84c2ac307a3de7580b6d8b2d3ce
Sammy Tam <sammytam2005@hotmail.com>
2019-05-23 17:00:53 -0700

    Update NotFound.vue

--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <b-container class="text-center">
-    <img src="../assets/error.jpg">
+    <img class="w-100" src="../assets/error.jpg">
     <p class="py-5">My Brain is Full of F*** is a catchphrase usually said in disbelief of something. Despite the first image macros<br> not featuring Chan, since 2009, it has come to be associated with an image of him looking perplexed.</p>
     <p class="h1">NOW GET OUTTA HERE</p>
   </b-container>
